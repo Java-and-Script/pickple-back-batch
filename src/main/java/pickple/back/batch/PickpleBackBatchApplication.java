87f0cfce82158eeef351463e66a6d735011bf1e5
changhyeon <changhyeon.h@kakao.com>
2023-12-04 21:19:43 +0900

    build: 프로젝트 초기화

---
+++ src/main/java/pickple/back/batch/PickpleBackBatchApplication.java
@@ -0,0 +1,13 @@
+package pickple.back.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickpleBackBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickpleBackBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pickple/back/batch/PickpleBackBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package pickple.back.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickpleBackBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
